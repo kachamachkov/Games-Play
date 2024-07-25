defc3d57f4775a5732f9821e1c01b5b17ae2fb95
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-25 19:36:17 +0300

    Refactor login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -2,25 +2,21 @@ import { useLogin } from "../../hooks/useAuth";
 import { useForm } from "../../hooks/useForm";
 import { useNavigate } from 'react-router-dom';
 
+const initialValues = { email: '', password: '' };
+
 const Login = () => {
-
     const login = useLogin();
-
     const navigate = useNavigate();
 
-    const { values, changeHandler, submitHandler } = useForm(
-        { email: '', password: '' },
-        async ({ email, password }) => {
-            try {
-                await login(email, password);
-
-                navigate('/');
-
-            } catch (err) {
-                console.log(err.message);
-            }
+    const loginHandler = async ({ email, password }) => {
+        try {
+            await login(email, password);
+            navigate('/');
+        } catch (err) {
+            console.log(err.message);
         }
-    );
+    };
+    const { values, changeHandler, submitHandler } = useForm(initialValues, loginHandler);
 
     return (
         <section id="login-page" className="auth">
